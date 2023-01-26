5f0efde5ee87820d587073434abf14836d397b37
susan <odiisusan@gmail.com>
2023-01-26 21:28:02 +0100

    env file added

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,6 +1,9 @@
-
 import './App.css';
+
 import Card from './Components/Card';
+
+require('dotenv').config()
+
 
 function App() {
   return (
---
+++ e-commerce/src/Components/Context/ProductContext.js
@@ -0,0 +1,10 @@
+import { createContext, useReducer } from 'react'
+
+function ProductContext ()  {
+  return (
+    <div>ProductContext</div>
+  )
+}
+
+export default ProductContext
+
